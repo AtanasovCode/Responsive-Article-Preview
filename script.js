315685f04bebd8537fc4ef2f50bea3cac81964c3
AtanasovCode <38294514+AtanasovCode@users.noreply.github.com>
2022-04-29 16:13:40 +0200

    Initial Commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const shareIcon = document.querySelector(".icon-share");
+const tooltips = document.querySelectorAll(".tooltip");
+const container = document.querySelector(".share-container");
+
+shareIcon.addEventListener("mouseover", () => {
+    tooltips.forEach(tooltip => {
+        tooltip.classList.toggle("active");
+        container.classList.add("active");
+    })
+})
+
+
+shareIcon.addEventListener("mouseleave", () => {
+    tooltips.forEach(tooltip => {
+        tooltip.classList.remove("active");
+        container.classList.remove("active");
+    })
+})
+
+//For Mobile Users
+
+shareIcon.addEventListener("click", () => {
+    tooltips.forEach(tooltip => {
+        tooltip.classList.toggle("active");
+        container.classList.add("active");
+    })
+})
+
+
+shareIcon.addEventListener("click", () => {
+    tooltips.forEach(tooltip => {
+        tooltip.classList.remove("active");
+        container.classList.remove("active");
+    })
+})
